8382e1677344c292fef19280c61fd8e72a48abeb
abhipsa9 <patroabhipsa9@gmail.com>
2022-06-04 12:44:56 +0530

    updated comments for the lines

--- src/TaskList.js
+++ src/TaskList.js
@@ -1,22 +1,26 @@
 import React, { useState } from 'react';
 
 function TaskList() {
-  // states for handling the below alignItems
-  //task lisk which has couple of preset data items
-  // state to handle input box value
-  // state to handle
+  //state for task lisk which has couple of preset data items
   const [inputValue, setInputValue] = useState('');
+  // state to handle input box value
   const [taskList, setTaskList] = useState(['a', 'b', 'c']);
+  // state to handle delete status to display status
   const [deleteClicked, setDeleteclicked] = useState(false);
 
+  //method to handle on change event for adding item
   const handleInput = (e) => {
     setInputValue(e.target.value);
   };
+
+  //method to handle submit click
   const handleSubmit = () => {
     setTaskList([...taskList, inputValue]);
     setInputValue('');
   };
 
+  //method to handle delete
+  // logic implemented as have taken an interval time to execute the line of code after 1 sec and simultaneously we are clearing of the interval so that it stop the execution once delete
   const handleDelete = (item) => {
     setDeleteclicked(true);
     var count = 0;
@@ -32,16 +36,21 @@ function TaskList() {
       }
     }, 1000);
   };
+
+  //method to handle sorting
   const handleSort = () => {
     let arr = taskList.sort();
     setTaskList([...arr]);
   };
+
   return (
     <div>
       <input type="text" onChange={handleInput} value={inputValue} />
       <button onClick={handleSubmit}>Submit</button>
       <button onClick={handleSort}>Sort</button>
+      {/* handles the message to show */}
       {deleteClicked ? <p>deleting..</p> : null}
+      {/* iterating the items to display from the list */}
       {taskList.map((item) => {
         return (
           <div>
